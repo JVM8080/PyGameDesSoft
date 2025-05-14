da370b67e59e8a1aa383ebe70939a65e8e78c89a
sophiamkalil <sophia.mkalil@gmail.com>
2025-05-14 01:54:23 -0300

    adicionei  base do level 2

--- src/levels/level_2.py
+++ src/levels/level_2.py
@@ -1,6 +1,7 @@
 import pygame
 from src.objects.player_stan import Player
-from config import HEIGHT
+from src.objects.platforms import Platform
+from config import *
 
 def run(screen):
     clock = pygame.time.Clock()
@@ -18,8 +19,15 @@ def run(screen):
         player.update(keys)
 
         screen.fill((100, 200, 100)) 
-        background = pygame.image.load('level2/cenario level 2.png').convert()
+        background = pygame.image.load("assets/images/level2/cenario level 2.jpeg").convert()
+        background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+        platforms = pygame.sprite.Group()
+        p1 = Platform("assets/images/level2/base.png", 0, HEIGHT-60, width=800, height=100)
+        platforms.add(p1)
+
         screen.blit(background, (0, 0)) 
+        platforms.draw(screen)
         player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
